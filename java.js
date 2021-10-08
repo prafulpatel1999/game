5babde204959d533f442220f1797c515259574bc
prafulpatel1999 <88098316+prafulpatel1999@users.noreply.github.com>
2021-10-08 08:42:22 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,32 @@
+let Num = prompt("type any number between '0-10'")
+
+if (Num == 1) {
+  alert("friends")
+}
+if (Num == 2) {
+  alert("special")
+}
+if (Num == 3) {
+  alert("i don't know you")
+}
+if (Num == 4) {
+  alert("i hate you")
+}
+if (Num == 5) {
+  alert("my comfort zone")
+}
+if (Num == 6) {
+  alert("i love you")
+}
+if (Num == 7) {
+  alert("bestie")
+}
+if (Num == 8) {
+  alert("crush")
+}
+if (Num == 9) {
+  alert("my happy place")
+}
+if (Num == 10) {
+  alert("my fighting partner")
+}
